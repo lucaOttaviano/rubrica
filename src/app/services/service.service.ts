c94f5dc1d2a99c6b6a71505769e9532c70ade7ac
lucaOttaviano <luca.ottaviano.lavoro@gmail.com>
2022-04-06 13:10:57 +0200

    +ultime modifiche engim

--- src/app/services/service.service.ts
+++ src/app/services/service.service.ts
@@ -4,18 +4,33 @@ import { Contact } from '../interfaces/contact';
   providedIn: 'root'
 })
 export class ServiceService {
-  array: Contact[]= []; // inizializzo l'array vuoto da riempire
+  array: Contact[]= [
+    {
+      firstname: "test",
+      lastname: "test", 
+      number: 308490283,
+    }
+  ]; // inizializzo l'array vuoto da riempire
   constructor() { }
   // aggiunge l'input dell'input.component all'array
   addContact(inputFirst: string, inputLast: string, inputNum: number){
-    // inizializzo una variabile contact con i tre parametri corrispondenti all'input dell'utente
-    console.log(typeof inputNum)
+    if (inputFirst == ""){
+      alert('name field empty');
+    }
+    else if (inputLast == ""){
+      alert('last name field empty');
+    }
+    else if (inputNum == 0){
+      alert('number field empty');
+    }
+    else{ 
+      // inizializzo una variabile contact con i tre parametri corrispondenti all'input dell'utente
       let contact = {
         firstname: inputFirst,
         lastname: inputLast,
         number: inputNum
       }
       this.array.push(contact); // pusho nell'array la variabile contact
-
+    }
 }
 } 
